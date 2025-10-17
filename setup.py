4c0ae08ac60e079c2719ac70e6a6478f3de1d6ea
TheBerriesLab <76480495+dabeLab@users.noreply.github.com>
2025-10-17 17:02:33 +0200

    Updated figures

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="mu_teg_sim",
-    version="1.0.0",
+    version="1.0.1",
     author="Davide Beretta",
     author_email="mail.davide.beretta+github@gmail.com",
     description="An app to simulate the device physics of micro Thermoelectric Generators",
     long_description="An app to simulate the device physics of micro Thermoelectric Generators (μTEGs). "
-                     "It calculates the power generated, the efficiency of conversion, the device resistance, the open circuit voltage, and the short circuit current "
-                     "per unit area as a function of the thermocouple length. "
+                     "It calculates the power generated, the efficiency of conversion, the device resistance, "
+                     "the open circuit voltage, the short circuit current and the closed circuit current per unit of "
+                     "device area as a function of the thermocouple length. "
                      "This app is designed for scientists, researchers, and engineers who want to simulate the device physics of μTEGs, "
                      "to analyze performance metrics and optimize designs for various applications.",
     url="https://github.com/BerriesLab/mu-teg-sim",
